85f9fccf03c0e92be51d42ce82b28077c02ad289
ajayjamage3 <ajayjamage3@gmail.com>
2023-04-21 12:31:01 +0530

    initil commit

--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 // See https://github.com/typicode/json-server#module
+
 const jsonServer = require('json-server')
+// const cors = require("cors")
+// server.use(cors())
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-const cors = require("cors")
-server.use(cors())
+
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
